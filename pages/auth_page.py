757fe5eb20fcb72128fcafc84f4b56846f1fc4f2
DianaSfl <eee.oo.ee@mail.ru>
2025-06-15 12:40:22 +0300

    Update auth_page.py

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,4 +1,6 @@
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.auth_locators import AuthLocators
 from pages.base_page import BasePage
@@ -12,4 +14,10 @@ class AuthPage(BasePage):
         self.fill(value=login_text, locator=AuthLocators.LOGIN)
         self.fill(value=password_text, locator=AuthLocators.PASSWORD)
         self.click(locator=AuthLocators.LOGIN_BUTTON)
+
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, "//h2[contains(text(), 'Список пользователей')]")),
+            message="Страница после авторизации не загрузилась"
+        )
+
         self.wait_and_click(locator=AuthLocators.LINK_ADD_USER)
