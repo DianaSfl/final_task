93efc75e3edf23afc5115e091236070ea970398b
DianaSfl <eee.oo.ee@mail.ru>
2025-06-18 20:54:27 +0300

    Add test

--- tests/test_add_user.py
+++ tests/test_add_user.py
@@ -106,7 +106,7 @@ class TestAddUser:
         logger.info(f"Result: {result}")
         assert "Пользователь успешно добавлен!" not in result, f"Пользователь был добавлен с невалидными данными в 'пол'"
 
-    def test_add_user_without_logging_in(self, logout, add_user_page, default_user_data):
+    def test_add_user_without_logging_in(self, auth_admin, logout, add_user_page, default_user_data):
 
         user_data = default_user_data.copy()
         add_user_page.add_user(
